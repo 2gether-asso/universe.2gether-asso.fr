5da8d78ab1b758e23caf2a8454fc1fbb6f02d092
Mati <matiboux@gmail.com>
2023-12-24 05:00:57 +0100

    Add i18n & Set default langage to french (#6)

---
+++ app/app/src/i18n/locales/en.ts
@@ -0,0 +1,21 @@
+const defaultLocale =
+[
+	'What is {0}?',
+	'Visit our website',
+	'Join our Discord',
+] as const
+
+type Keys = typeof defaultLocale[number]
+type Type = { [key in Keys]: key }
+
+// Default locale uses the key as the value
+const locale = defaultLocale
+	.reduce<Type>((acc, key) =>
+		{
+			(acc as any)[key] = key
+			return acc
+		},
+		{} as Type,
+	)
+
+export default locale as Readonly<typeof locale>
