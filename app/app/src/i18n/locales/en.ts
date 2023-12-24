d9997283f4402cb93507b4f01b050e408ae45614
Matiboux <matiboux@gmail.com>
2023-12-24 06:37:51 +0100

    Add description

--- app/app/src/i18n/locales/en.ts
+++ app/app/src/i18n/locales/en.ts
@@ -3,6 +3,9 @@ const defaultLocale =
 	'What is {0}?',
 	'Visit our website',
 	'Join our Discord',
+	'You want to understand 2GETHER references, know why the Posson exists, who leads the dance at the lighthouse, or even why barrels are well loaded?',
+	'Well, dive into 2GETHER Universe!',
+	'An interactive platform giving you access to several resources of the 2GETHER universe: animations, comics, pieces of stories from a lost journal, and much more!',
 ] as const
 
 type Keys = typeof defaultLocale[number]
